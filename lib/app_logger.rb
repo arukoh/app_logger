39d439d32a012237af22a24f07be845969fbc38b
arukoh <arukoh10@gmail.com>
2016-03-24 17:38:53 +0900

    Fix a bug of force logger

--- lib/app_logger.rb
+++ lib/app_logger.rb
@@ -15,11 +15,12 @@ module AppLogger
 
     def new_force_formatted_logger(logdev, level: nil, formatter: nil, console: false, severity: Logger::UNKNOWN)
       logger = new_logger(logdev, level: level, formatter: formatter, console: console)
+      logger.instance_variable_set(:@_severity, severity)
       logger.instance_eval do |obj|
         class << self
           define_method :<< do |msg|
-            return true if @logdev.nil? or severity < @level
-            @logdev.write(format_message(format_severity(severity), Time.now, @progname, msg))
+            return true if @logdev.nil? or @_severity < @level
+            @logdev.write(format_message(format_severity(@_severity), Time.now, @progname, msg))
             true
           end
         end
